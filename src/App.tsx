8ca91c11f25eff584eca218d6de1174b91c1c075
Ameera <amiraelalfe3@gmail.com>
2026-04-30 15:35:20 +0300

    ;;

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import About from './components/About'
+
 import Skills from './components/Skills'
 import Experience from './components/Experience'
 import Projects from './components/Projects'
